44994132b9632055b9fad7f76e0722a40acc398c
SiddharthaAnand <siddharthalibra13@gmail.com>
2017-09-24 00:08:43 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="imdbwrap",
+	  version="0.1",
+	  description="Imdb: collection of movies",
+	  url="https://github.com/SiddharthaAnand/imdb_wrap",
+	  author="siddhartha",
+	  author_email="siddharthalibra13@gmail.com",
+	  license="MIT",
+	  packages=['imdb_wrap'],
+	  zip_safe=False)
