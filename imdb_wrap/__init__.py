d2ef7c5935c3af616696959592c247e3df3f07b1
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-11-10 23:54:26 +0530

    Remove unused methods

--- imdb_wrap/__init__.py
+++ imdb_wrap/__init__.py
@@ -1,6 +1,5 @@
 from .imdb_engine import scraper
 from .imdb_engine import upcoming_movie_name
-from .imdb_engine import movie_information
-from .imdb_engine import upcoming_movie_description
+
 
 
--- imdb_wrap/imdb_engine.py
+++ imdb_wrap/imdb_engine.py
@@ -6,19 +6,8 @@ from bs4 import BeautifulSoup
 month_num = {name:num for num, name in enumerate(calendar.month_abbr) if num}
 
 
-def validate(var, type):
-	pass
-
-
 def create_bs(response):
 	return BeautifulSoup(response.text.encode('utf-8'))
-
-
-def movie_information(soup):
-	movie_info_odd=soup.findAll('div', attrs={'class':'list_item odd'})
-	movie_info_even=soup.findAll('div', attrs={'class':'list_item even'})
-	# Combine both in one list
-	return movie_info_odd, movie_info_even
 
 
 def upcoming_movie_name(soup):
@@ -34,26 +23,16 @@ def upcoming_movie_name(soup):
 	return movie_names
 
 
-def upcoming_movie_description(movie_info):
-	description = {}
-	for movie_data in movie_info:
-		movie_name = movie_data.find('h4', attrs={'itemprop':'name'}).text.encode('utf-8').strip()
-		movie_desc = movie_data.find('div', attrs={'class':'outline'}).text.encode('utf-8').strip()
-		description[movie_name] = movie_desc
-
-	return description
-
-
-def scraper(month='Jan'):
+def scraper(month='Jan', year=2019):
 	if month_num[month] < 10:
-		url = "http://www.imdb.com/movies-coming-soon/2017-0" + str(month_num[month])
+		url = "http://www.imdb.com/movies-coming-soon/{year}-0{month}".format(year=year, month=month_num[month])
 	else:
-		url = "http://www.imdb.com/movies-coming-soon/2017-" + str(month_num[month])
+		url = "http://www.imdb.com/movies-coming-soon/{year}-{month}".format(year=year, month=month_num[month])
 	try:
 		response = requests.get(url)
 		print response
 	except Exception as e:
-		pass
+		print e
 	soup = create_bs(response)
 	return soup
 
