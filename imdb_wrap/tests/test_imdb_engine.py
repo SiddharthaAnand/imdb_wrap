cbaaf8267be0d2ef5ef36442367933ad1ef882a4
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-11-11 00:03:23 +0530

    Add variants of tests for method scraper

--- imdb_wrap/tests/test_imdb_engine.py
+++ imdb_wrap/tests/test_imdb_engine.py
@@ -2,7 +2,14 @@ from unittest import TestCase
 from bs4 import BeautifulSoup
 import imdb_wrap
 
+
 class TestEngine(TestCase):
 	def test_scraper(self):
 		soup = imdb_wrap.scraper('Jan')
+		self.assertTrue(isinstance(soup, BeautifulSoup))
+
+		soup = imdb_wrap.scraper('Jan', 2019)
+		self.assertTrue(isinstance(soup, BeautifulSoup))
+
+		soup = imdb_wrap.scraper(year=2019)
 		self.assertTrue(isinstance(soup, BeautifulSoup))
