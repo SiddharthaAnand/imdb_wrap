2d30943767fae30a25322b5a25df80db2c41a652
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-11-11 00:05:56 +0530

    rename lib name

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-setup(name="imdbwrap",
-	  version="0.1",
-	  description="Imdb: collection of movies",
-	  url="https://github.com/SiddharthaAnand/imdb_wrap",
-	  author="siddhartha",
-	  author_email="siddharthalibra13@gmail.com",
-	  license="MIT",
-	  packages=['imdb_wrap'],
-	  test_suite='nose.collector',
-	  tests_require=['nose'],
-	  install_requires=['bs4', 'requests'],
-	  zip_safe=False)
+
+setup(name="imdb_wrap",
+      version="0.1",
+      description="Imdb: collection of movies",
+      url="https://github.com/SiddharthaAnand/imdb_wrap",
+      author="siddhartha",
+      author_email="siddharthalibra13@gmail.com",
+      license="MIT",
+      packages=['imdb_wrap'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      install_requires=['bs4', 'requests'],
+      zip_safe=False)
