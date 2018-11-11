3b3946932498579cbf6384e80931e15d2154139c
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-11-11 18:57:30 +0530

    Generate python package distrbution

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
-from setuptools import setup
+import setuptools
 
-
-setup(name="imdb_wrap",
+setuptools.setup(name="imdb_wrap",
       version="0.1",
       description="Imdb: collection of movies",
       url="https://github.com/SiddharthaAnand/imdb_wrap",
       author="siddhartha",
       author_email="siddharthalibra13@gmail.com",
       license="MIT",
-      packages=['imdb_wrap'],
+      packages=setuptools.find_packages(),
       test_suite='nose.collector',
       tests_require=['nose'],
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
       install_requires=['bs4', 'requests'],
-      zip_safe=False)
+      zip_safe=False,
+     classifiers=[
+                 "Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                  ],
+     )
